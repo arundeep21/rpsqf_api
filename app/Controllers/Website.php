2652bd1a28695cb0431a83e045dfe8c3cc8e5be7
unknown <arundeep@ratepersqft.com>
2022-11-16 11:32:11 +0530

    website

--- app/Controllers/Website.php
+++ app/Controllers/Website.php
@@ -23,43 +23,23 @@ class Website extends ResourceController
     }
 
 
-//get single Property
-    
-public function our_brands()
-{
-    
-    $mongo=new DatabaseConnector();
-
-    $db=$mongo->getDatabase();
-    // $builder = $db->our_brands;
-    // $builder->find();
-    // print_r($builder);
-    // exit(); 
-    $collection=$db->our_brands;
-    // $filter = array('id'=> '1' );
-
-    $filter = [];
-    // $query = new \MongoDB\Driver\Query($filter);
-
-    $cursor = $collection->find($filter);   
-
-    $return=array();
-      foreach ($cursor as $document) {
-        $return[]=$document;
-     }
-
-     return $this->respond($return, 200);
-    // print_r($cursor);
-    
-}
-
-public function service_list()
-{
+    //get single Property
+        
+    public function our_brands()
+    {
+        
         $mongo=new DatabaseConnector();
-        $db=$mongo->getDatabase();
 
-        $collection=$db->our_services;
-       // $filter = array('category' => new \MongoDB\BSON\Regex($category));
+        $db=$mongo->getDatabase();
+        // $builder = $db->our_brands;
+        // $builder->find();
+        // print_r($builder);
+        // exit(); 
+        $collection=$db->clients;
+        // $filter = array('id'=> '1' );
+
+        $filter = [];
+        // $query = new \MongoDB\Driver\Query($filter);
 
         $cursor = $collection->find();   
 
@@ -67,41 +47,134 @@ public function service_list()
           foreach ($cursor as $document) {
             $return[]=$document;
          }
+
          return $this->respond($return, 200);
-    }    
+        // print_r($cursor);
+        
+    }
+
+    public function service_list()
+    {
+            $mongo=new DatabaseConnector();
+            $db=$mongo->getDatabase();
+
+            $collection=$db->our_services;
+           // $filter = array('category' => new \MongoDB\BSON\Regex($category));
+
+            $cursor = $collection->find();   
+
+            $return=array();
+              foreach ($cursor as $document) {
+                $return[]=$document;
+             }
+             return $this->respond($return, 200);
+        }    
 
 
-public function third_party_service_list()
-{
-        $mongo=new DatabaseConnector();
-        $db=$mongo->getDatabase();
+    public function third_party_service_list()
+    {
+            $mongo=new DatabaseConnector();
+            $db=$mongo->getDatabase();
 
-        $collection=$db->thirdparty_services;
-        //$filter = array('category' => new \MongoDB\BSON\Regex($category));
+            $collection=$db->thirdparty_services;
+            //$filter = array('category' => new \MongoDB\BSON\Regex($category));
 
-        $cursor = $collection->find();   
+            $cursor = $collection->find();   
 
-        $return=array();
-          foreach ($cursor as $document) {
-            $return[]=$document;
-         }
-         return $this->respond($return, 200);
-    }   
+            $return=array();
+              foreach ($cursor as $document) {
+                $return[]=$document;
+             }
+             return $this->respond($return, 200);
+        }   
 
-    public function choose_us()
-{
-        $mongo=new DatabaseConnector();
-        $db=$mongo->getDatabase();
 
-        $collection=$db->whychoose_us;
-        //$filter = array('category' => new \MongoDB\BSON\Regex($category));
+    public function third_party_service_providers($category)
+    {
+            $mongo=new DatabaseConnector();
+            $db=$mongo->getDatabase();
 
-        $cursor = $collection->find();   
+            $collection=$db->third_party_service_providers;
+            $filter = array('category' => $category);
 
-        $return=array();
-          foreach ($cursor as $document) {
-            $return[]=$document;
-         }
-         return $this->respond($return, 200);
-    }  
+            $cursor = $collection->find($filter);   
+
+            $return=array();
+              foreach ($cursor as $document) {
+                $return[]=$document;
+             }
+             return $this->respond($return, 200);
+        }   
+
+      public function choose_us()
+      {
+              $mongo=new DatabaseConnector();
+              $db=$mongo->getDatabase();
+
+              $collection=$db->whychoose_us;
+              //$filter = array('category' => new \MongoDB\BSON\Regex($category));
+
+              $cursor = $collection->find();   
+
+              $return=array();
+                foreach ($cursor as $document) {
+                  $return[]=$document;
+               }
+               return $this->respond($return, 200);
+      }  
+
+
+      public function service_provider_detail($id="")
+      {
+              $mongo=new DatabaseConnector();
+              $db=$mongo->getDatabase();
+
+              $collection=$db->third_party_service_providers;
+              $filter = array('id' => intval($id));
+
+              $cursor = $collection->find($filter);   
+
+              $return=array();
+                foreach ($cursor as $document) {
+                  $return[]=$document;
+               }
+               return $this->respond($return, 200);
+          }
+
+      public function testimonials()
+      {
+              $mongo=new DatabaseConnector();
+              $db=$mongo->getDatabase();
+
+              $collection=$db->testimonials;
+             
+
+              $cursor = $collection->find();   
+
+              $return=array();
+                foreach ($cursor as $document) {
+                  $return[]=$document;
+               }
+               return $this->respond($return, 200);
+          }
+
+      public function aboutus()
+      {
+              $mongo=new DatabaseConnector();
+              $db=$mongo->getDatabase();
+
+              $collection=$db->static_pages;             
+
+
+              $filter = array('slug' => 'about-us');
+
+              $cursor = $collection->find($filter);   
+
+              $return=array();
+                foreach ($cursor as $document) {
+                  $return[]=$document;
+               }
+               return $this->respond($return, 200);
+          }
+
 }
